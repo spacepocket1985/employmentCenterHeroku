3c2120a9472807ed0385d1b04167515a86419bbd
Space Pocket <space1985pocket@gmail.com>
2024-05-24 14:38:55 +0300

    refactor: del package-lock

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { app, port } from './app';
-import { startDB } from './utils/startDB';
+const { app, port } = require('./app');
+const { startDB } = require('./utils/startDB');
 
 const startApp = async (): Promise<void> => {
   await startDB();
