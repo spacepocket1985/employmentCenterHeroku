9c084eb9c3e6eda987314a7d86530cf4d372ee5f
Space Pocket <space1985@pocket@gmail.com>
2024-06-17 20:52:34 +0300

    refactor: update mongoURI

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,5 +1,7 @@
 const keys = {
-  mongoURI: process.env.mongoURI || "mongodb://localhost:27017/tec2Center",
+  mongoURI:
+    process.env.mongoURI ||
+    "mongodb://u1diwflhcp25bndd78fw:asixrWXcrXBD5yRksYJb@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bogiocvbowjmkmj?replicaSet=rs0",
   jwt: "tec2-center-jwt",
 };
 
